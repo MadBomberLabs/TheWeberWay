829fdc9987d37ec4fabe060c6553a732264bdd9d
Brian Weber <brianweber@roadrunner.com>
2017-02-06 18:31:52 -0500

    Testing things out
    Testing out.  added text to commercial body text.

--- templates/com_body.php
+++ templates/com_body.php
@@ -2,4 +2,8 @@
 
 This is com body text
 
+This is modification to the commercial body text to test out git and how branches and pull requests work:)
+
+Let me know if you make this live so I can check it out and then I will work on real content to put here.
+
 </div>
