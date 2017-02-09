078ccd0491ca3361909fa3c4395989811fee7d64
Brian Weber <brianweber@roadrunner.com>
2017-02-09 18:51:31 -0500

    1st changes on Commercial
    I didn’t realize but I need to move this to industrial but I can not
    see what I am doing without a testing server.  Can you make this pull
    live and help with server so I can send you better stuff??

--- templates/com_body.php
+++ templates/com_body.php
@@ -1,9 +1,33 @@
-<div>
 
-This is com body text
+<img class="homePhoto" src="../images/stockPhotos/skidSteer.jpg" alt="skidsteer" />
 
-This is modification to the commercial body text to test out git and how branches and pull requests work:)
+<!-- Paragraph 1 -->              
+<div class="col-md-12 col-lg-4">
+	<strong>Thank you</strong> for visiting our site! <br>With Weber there is truly no
+     limit to what we can do. Whatever the project, we can transport it, rig it, pipe 
+     it duct it and wire it. With our extensive in-house capabilities and strategic 
+     partners, you can count on Atlas to do what it takes to get the job done right.<br><br>
+</div>
 
-Let me know if you make this live so I can check it out and then I will work on real content to put here.
+<!-- Paragraph 2 -->
+<div class="col-md-12 col-lg-4">
+	<strong>The Weber Way</strong> commits Weber Contracting to satety!
+    Since its inception, Weber has had a very strong commitment to safety. 
+    We have one of the best safety records in the business.<br><br>
+</div>
 
+<!-- Paragraph 3 -->
+<div class="col-md-12 col-lg-4">
+Rigging & Machinery Installation
+
+	The <strong>employees</strong> of Weber Contracting are experienced and professional. We look forward to 
+	working with new and current clients, as well as collaborating with satisfied customers that have experienced 
+	The Weber Way in the past!<br><br>
+</div>
+
+<!-- Paragraph 4 -->
+<div class="col-md-12"><br>
+	Give our office a call today at <strong>716-791-8423</strong>!<br>
+	We strive to return telephone calls the same day, or by 9 a.m. the following morning. We look 
+	forward to hearing from you!<br><br>
 </div>
